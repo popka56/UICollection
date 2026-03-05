63c718116e8a09a5d269b008ebe041f989fe26a9
popka56 <popka56@github.com>
2026-03-05 11:54:02 +0100

    Add SIVA console project

---
+++ BlazorUICollection/Pages/SIVA.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorUICollection.Pages
+{
+    public partial class SIVA : ComponentBase
+    {
+        private bool _isInitializing = true;
+
+        protected override async Task  OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await Task.Delay(5000);
+                _isInitializing = false;
+                StateHasChanged();
+            }
+        }
+    }
+}
